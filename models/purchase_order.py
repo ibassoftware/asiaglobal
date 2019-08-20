c05091231dc8c9be951596d85381d457dd36cee3
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2019-08-20 18:12:15 +0800

    Added Ship Via Added Department using analytic tag Updated PO templates

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,12 +1,20 @@
-# from odoo import models, fields, api, _ 
+from odoo import models, fields, api, _ 
 
-# class PurchaseOrder(models.Model):
-# 	_inherit = 'purchase.order'
+class PurchaseOrder(models.Model):
+	_inherit = 'purchase.order'
 
-# 	department = fields.Char(compute='_get_analytic_tags')
+	department = fields.Char(compute='_get_analytic_tags')
+	ship_via = fields.Char(string='Ship Via')
 
-# 	@api.multi
-# 	def _get_analytic_tags(self):
-# 		for order in self:
-# 			tag_ids = order.mapped('order_line').mapped('analytic_tag_ids')[0]
-# 			order.department = tag_ids.name
+	@api.multi
+	def _get_analytic_tags(self):
+		for order in self:
+			department = ''
+			tag_ids = order.mapped('order_line').mapped('analytic_tag_ids')
+			for tag in tag_ids:
+				if department:
+					department += ', '
+					department += tag.name
+				else:
+					department = tag.name
+			order.department = department
