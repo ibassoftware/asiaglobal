a75b8c1542d01e36276227c69e296894586cfaad
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2020-06-04 10:14:55 +0800

    Added new report layout for payment receipt; Added Source of Sale; Modified Expiration Date

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,4 +3,16 @@ from odoo import models, fields, api, _
 class SaleOrder(models.Model):
 	_inherit = 'sale.order'
 
+	# NEW FIELDS
 	jo_id = fields.Many2one('asiaglobal.job_order', string='Job Order')
+	sale_source = fields.Selection([
+		('exhibit', 'Exhibit'),
+		('new', 'New Client'),
+		('referral', 'Referral'),
+		('saturation', 'Saturation'),
+		('old', 'Old Customer'),
+		('other', 'Others'),
+	], string='Source of Sale')
+
+	# OVERRIDE
+	validity_date = fields.Date(states={'draft': [('readonly', False)], 'manager_approval': [('readonly', False)], 'admin_approval': [('readonly', False)], 'approved': [('readonly', False)], 'sent': [('readonly', False)]},)
