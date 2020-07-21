7abec0d6d6083751c835ae69a3a64ca93a2fbc7f
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2020-07-21 14:47:43 +0800

    Updated compute amount check words

--- models/account_payment.py
+++ models/account_payment.py
@@ -7,20 +7,20 @@ _logger = logging.getLogger(__name__)
 class AccountRegisterPayments(models.TransientModel):
 	_inherit = "account.register.payments"
 
-	@api.multi
-	@api._onchange_amount('amount')
+	@api.onchange('amount')
 	def _onchange_amount(self):
-		for rec in self:
-			whole = num2words(int(rec.amount)) + ' Pesos '
-			whole = whole.replace(' and ',' ')
-			if "." in str(rec.amount): # quick check if it is decimal
-				decimal_no = str(rec.amount).split(".")[1]
-				if len(decimal_no) == 1:
-					decimal_no = decimal_no + "0"
-			if decimal_no:
-					whole = whole + "and " + decimal_no + '/100'
-			whole = whole.replace(',','')
-			rec.check_amount_in_words = whole.upper() + " ONLY"
+		if hasattr(super(AccountRegisterPayments, self), '_onchange_amount'):
+			super(AccountRegisterPayments, self)._onchange_amount()
+		whole = num2words(int(self.amount)) + ' Pesos '
+		whole = whole.replace(' and ',' ')
+		if "." in str(self.amount): # quick check if it is decimal
+			decimal_no = str(self.amount).split(".")[1]
+			if len(decimal_no) == 1:
+				decimal_no = decimal_no + "0"
+		if decimal_no:
+				whole = whole + "and " + decimal_no + '/100'
+		whole = whole.replace(',','')
+		self.check_amount_in_words = whole.upper() + " ONLY"
 
 	def _prepare_payment_vals(self, invoices):
 		res = super(AccountRegisterPayments, self)._prepare_payment_vals(invoices)
@@ -36,8 +36,9 @@ class AccountPayment(models.Model):
 	amount_in_words = fields.Char(string='Amount In Words', compute='_onchange_amount')
 
 	@api.multi
-	@api._onchange_amount('amount','currency_id')
+	@api.onchange('amount','currency_id')
 	def _onchange_amount(self):
+		res = super(AccountPayment, self)._onchange_amount()
 		for rec in self:
 			whole = num2words(int(rec.amount)) + ' Pesos '
 			whole = whole.replace(' and ',' ')
@@ -50,15 +51,13 @@ class AccountPayment(models.Model):
 			whole = whole.replace(',','')
 			# rec.amount_in_words = whole.upper() + " ONLY"
 			rec.check_amount_in_words = whole.upper() + " ONLY"
+		return res
 
 	@api.multi
 	def compute_check_amount_in_words(self):
 		for record in self:
 			if not record.check_amount_in_words or not record.amount_in_words:
-				# check_amount_in_words = record.currency_id.amount_to_text(record.amount)
-				# record.check_amount_in_words = check_amount_in_words
 				record._onchange_amount()
-
 		return True
 
 	# Override to remove asterisk
